41eb9024c3418c8d30efc6e90cf0089e7a71189a
knutbo <knutbo@7b885804-ab2d-0410-b250-0b2d121ae3f5>
2007-06-25 17:17:09 +0000

    More work with Trust view - edit part.
    git-svn-id: https://accountserver.googlecode.com/svn/trunk@66 7b885804-ab2d-0410-b250-0b2d121ae3f5

--- RegnskapServer/services/registers/trustaction.php
+++ RegnskapServer/services/registers/trustaction.php
@@ -7,15 +7,21 @@
 include_once ("../../conf/AppConfig.php");
 include_once ("../../classes/util/DB.php");
 include_once ("../../classes/accounting/accounttrustaction.php");
+include_once ("../../classes/accounting/accounttrust.php");
 
 $action = array_key_exists("action", $_REQUEST) ? $_REQUEST["action"] : "all";
 
 $db = new DB();
 $accountAction = new AccountTrustAction($db);
+$accTrust = new AccountTrust($db);
 
 switch ($action) {
 	case "all" :
-		echo json_encode($accountAction->getAll());
+        $res = array();
+        $res["actions"] = $accountAction->getAll();
+        $res["types"] = $accTrust->getFondtypes();
+         
+		echo json_encode($res);
 		break;
 	default :
 		die("Unknown action" + $action);

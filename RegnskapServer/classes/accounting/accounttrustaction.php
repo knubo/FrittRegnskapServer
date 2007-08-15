b870c5e53ec0ecb1084ead877a73545410f23634
knutbo <knutbo@7b885804-ab2d-0410-b250-0b2d121ae3f5>
2007-08-15 12:28:50 +0000

    Functionallity for editing trust actions.
    git-svn-id: https://accountserver.googlecode.com/svn/trunk@119 7b885804-ab2d-0410-b250-0b2d121ae3f5

--- RegnskapServer/classes/accounting/accounttrustaction.php
+++ RegnskapServer/classes/accounting/accounttrustaction.php
@@ -12,10 +12,20 @@ class AccountTrustAction {
 	public $actionfond;
 	public $debetpost;
 	public $creditpost;
+    public $description;
+    public $defaultdesc;
     public $id;
     
-	function AccountTrustAction($db) {
+	function AccountTrustAction($db, $fond = 0, $description =0, $defaultdesc = 0, $actionclub = 0, $actionfond = 0, $debetpost=0, $creditpost = 0, $id = 0) {
 		$this->db = $db;
+        $this->fond = $fond;
+        $this->description = $description;
+        $this->defaultdesc = $defaultdesc;
+        $this->actionclub = $actionclub;
+        $this->actionfond = $actionfond;
+        $this->debetpost = $debetpost;
+        $this->creditpost = $creditpost;
+        $this->id = $id;
 	}
 
 	function getAll() {
@@ -23,6 +33,21 @@ class AccountTrustAction {
 
 		return $prep->execute();
 	}
+
+    function save() {
+        if($this->id) {
+        	$prep = $this->db->prepare("update " . AppConfig :: DB_PREFIX . "fond_action set fond = ?, description = ?, defaultdesc = ?, actionclub = ?, actionfond = ?, debetpost = ?, creditpost = ? where id = ?");
+            $prep->bind_params("sssiiiii", $this->fond, $this->description, $this->defaultdesc, $this->actionclub, $this->actionfond, $this->debetpost, $this->creditpost, $this->id);
+            $prep->execute();
+            return $db->affected_rows();        	
+        } else {
+        	$prep = $this->db->prepare("insert into " . AppConfig :: DB_PREFIX . "fond_action set fond = ?, description = ?, defaultdesc = ?, actionclub = ?, actionfond = ?, debetpost = ?, creditpost = ?");
+            $prep->bind_params("sssiiii", $this->fond, $this->description, $this->defaultdesc, $this->actionclub, $this->actionfond, $this->debetpost, $this->creditpost);
+            $this->id = $this->db->insert_id();
+            $prep->execute();
+            return 1;            
+        }
+    }
 
 	function load($id) {
 		$prep = $this->db->prepare("select * from " . AppConfig :: DB_PREFIX . "fond_action where id = ?");
@@ -56,7 +81,6 @@ class AccountTrustAction {
 		$newEntry = new AccountTrust($this->db, $this->fond, $desc, $amount * $this->actionfond, $amount * $this->actionclub, 0, 0, $accountLine != null ?  $accountLine->getId() : 0);
 		$newEntry->setDate($day, $month, $year);
 		$newEntry->store();
-        
         
 		return 1;
 	}
--- RegnskapServer/services/registers/trustaction.php
+++ RegnskapServer/services/registers/trustaction.php
@@ -1,4 +1,6 @@
 <?php
+
+
 /*
  * Created on Jun 22, 2007
  *
@@ -11,23 +13,41 @@ include_once ("../../classes/accounting/accounttrust.php");
 include_once ("../../classes/auth/RegnSession.php");
 
 $action = array_key_exists("action", $_REQUEST) ? $_REQUEST["action"] : "all";
+$id = array_key_exists("id", $_REQUEST) ? $_REQUEST["id"] : null;
+$trust = array_key_exists("trust", $_REQUEST) ? $_REQUEST["trust"] : null;
+$description = array_key_exists("description", $_REQUEST) ? $_REQUEST["description"] : null;
+$defaultdesc = array_key_exists("defaultdesc", $_REQUEST) ? $_REQUEST["defaultdesc"] : null;
+$clubaction = array_key_exists("clubaction", $_REQUEST) ? $_REQUEST["clubaction"] : null;
+$trustaction = array_key_exists("trustaction", $_REQUEST) ? $_REQUEST["trustaction"] : null;
+$debetpost = array_key_exists("debetpost", $_REQUEST) ? $_REQUEST["debetpost"] : null;
+$creditpost = array_key_exists("creditpost", $_REQUEST) ? $_REQUEST["creditpost"] : null;
 
 $db = new DB();
 $regnSession = new RegnSession($db);
 $regnSession->auth();
 
-$accountAction = new AccountTrustAction($db);
-$accTrust = new AccountTrust($db);
 
 switch ($action) {
 	case "all" :
-        $res = array();
-        $res["actions"] = $accountAction->getAll();
-        $res["types"] = $accTrust->getFondtypes();
-         
+        $accountAction = new AccountTrustAction($db);
+        $accTrust = new AccountTrust($db);
+		$res = array ();
+		$res["actions"] = $accountAction->getAll();
+		$res["types"] = $accTrust->getFondtypes();
+
 		echo json_encode($res);
 		break;
-	default :  
+	case "save" :
+       $accountAction = new AccountTrustAction($db, $trust,$description, $defaultdesc, $clubaction, $trustaction, $debetpost, $creditpost, $id);
+       $res = $accountAction->save();
+       
+       if($res) {
+       	    echo json_encode($accountAction);
+       } else {
+       	    echo "0";
+       }
+       break;
+	default :
 		die("Unknown action" + $action);
 		break;
 }

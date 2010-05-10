5a934d765fa283fdc4d7055f95da8dc70cc8403d
knutbo <knutbo@7b885804-ab2d-0410-b250-0b2d121ae3f5>
2010-05-10 18:09:17 +0000

    Better error handling.
    git-svn-id: https://accountserver.googlecode.com/svn/trunk@247 7b885804-ab2d-0410-b250-0b2d121ae3f5

--- RegnskapServer/services/backup.php
+++ RegnskapServer/services/backup.php
@@ -17,49 +17,53 @@ $backup = new BackupDB($db);
 $regnSession->auth();
 $acStandard = new AccountStandard($db);
 
+date_default_timezone_set(AppConfig::TIMEZONE);
+
 switch ($action) {
-	case "tables" :
-		echo json_encode($backup->tables());
-		break;
+    case "tables" :
+        echo json_encode($backup->tables());
+        break;
 
-	case "init" :
-		$acStandard->setValue("BACKUP_TIME", date("m.d.Y H:i"));
-		/* Fallthrough */
-	case "delete" :
-		$path = "../backup/";
-		$handle = opendir($path);
-		for (; false !== ($file = readdir($handle));)
-			if ($file != "." && $file != "..")
-				unlink($path . $file);
-		closedir($handle);
+    case "init" :
+        $acStandard->setValue("BACKUP_TIME", date("m.d.Y H:i"));
+        /* Fallthrough */
+    case "delete" :
+        $path = "../backup/";
+        $handle = opendir($path);
+        for (; false !== ($file = readdir($handle));) {
+            if (strncasecmp($file, ".", 1) != 0) {
+                unlink($path . $file);
+            }
+        }
+        closedir($handle);
 
-		$res = array ();
-		$res["result"] = 1;
-		echo json_encode($res);
-		break;
-	case "backup" :
-		$res = array ();
-		$res["result"] = json_encode($backup->backup($table)) ? 1 : 0;
-		echo json_encode($res);
-		break;
-	case "info" :
-		$res = array ();
-		$res["last_backup"] = $acStandard->getOneValue("BACKUP_TIME");
+        $res = array ();
+        $res["result"] = 1;
+        echo json_encode($res);
+        break;
+    case "backup" :
+        $res = array ();
+        $res["result"] = json_encode($backup->backup($table)) ? 1 : 0;
+        echo json_encode($res);
+        break;
+    case "info" :
+        $res = array ();
+        $res["last_backup"] = $acStandard->getOneValue("BACKUP_TIME");
         if(file_exists("../backup/backup.zip")) {
             $res["backup_file"] = date("m.d.Y H:i", filemtime("../backup/backup.zip"));
         }
-		echo json_encode($res);
-		break;
-	case "zip" :
+        echo json_encode($res);
+        break;
+    case "zip" :
         $res = array ();
         $res["result"] = json_encode($backup->zip($table)) ? 1 : 0;
         echo json_encode($res);
         break;
-	case "get" :
+    case "get" :
         $backupFileName = "backupAccounting".date("m-d-Y").".zip";
         header('Content-type: octet-stream');
         header('Content-Disposition: attachment; filename="'.$backupFileName);
         readfile("../backup/backup.zip");
-		break;
+        break;
 }
 ?>

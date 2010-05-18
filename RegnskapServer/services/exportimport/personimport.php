1061d2eeae3f0fda61e3afeeefbc2cd1aac87726
knutbo <knutbo@7b885804-ab2d-0410-b250-0b2d121ae3f5>
2010-05-18 18:23:11 +0000

    Work in progress with import of persons from csv file.
    git-svn-id: https://accountserver.googlecode.com/svn/trunk@253 7b885804-ab2d-0410-b250-0b2d121ae3f5

---
+++ RegnskapServer/services/exportimport/personimport.php
@@ -0,0 +1,59 @@
+<?php
+
+include_once ("../../conf/AppConfig.php");
+include_once ("../../classes/util/DB.php");
+include_once ("../../classes/util/strings.php");
+include_once ("../../classes/util/logger.php");
+include_once ("../../classes/auth/RegnSession.php");
+include_once ("../../classes/auth/Master.php");
+
+$action = array_key_exists("action", $_REQUEST) ? $_REQUEST["action"] : "list";
+$delimiter = array_key_exists("delimiter", $_REQUEST) ? $_REQUEST["delimiter"] : "";
+$db = new DB();
+$logger = new Logger($db);
+$regnSession = new RegnSession($db);
+$regnSession->auth();
+
+
+
+switch($action) {
+    case "findfields":
+        $content = file_get_contents($_FILES['uploadFormElement']['tmp_name']);
+
+        $lines = explode("\n", $content);
+         
+        echo "<table class=\"importtable\">\n";
+
+        $i = 0;
+        $headers = 0;
+        foreach($lines as $one) {
+
+
+            if(!$headers) {
+                $colCount = preg_match_all("/(\Q".$delimiter."\E)(?=(?:[^\"]|\"[^\"]*\")*$)/", $one, $matches);
+                echo "<tr><td><!-- --></td>";
+
+                for($col = 0; $col <= $colCount; $col++) {
+                    echo "<td><div id=\"col".$col."\"><!-- --></div></td>";
+                }
+                echo "</tr>";
+
+                $headers = 1;
+            }
+
+            $style = $i % 6 >= 3 ? "line1" : "line2";
+
+            echo "<tr class=\"$style\"><td><div id=\"remove".$i."\"><!-- --></div></td><td>";
+            $one = preg_replace("/(\Q".$delimiter."\E)(?=(?:[^\"]|\"[^\"]*\")*$)/", "</td><td>", $one);
+            echo preg_replace("/\"/", "", $one);
+            echo "</td></tr>\n";
+
+            $i++;
+        }
+        echo "</table>\n";
+         
+        break;
+}
+
+
+?>

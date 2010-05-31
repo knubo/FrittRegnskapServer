cdd17e742f3bfb34acca57f47bee0d7386622b6d
knutbo <knutbo@7b885804-ab2d-0410-b250-0b2d121ae3f5>
2010-05-31 10:42:45 +0000

    Eksport av personer.
    git-svn-id: https://accountserver.googlecode.com/svn/trunk@257 7b885804-ab2d-0410-b250-0b2d121ae3f5

---
+++ RegnskapServer/services/exportimport/personexport.php
@@ -0,0 +1,67 @@
+<?php
+
+include_once ("../../conf/AppConfig.php");
+include_once ("../../classes/util/DB.php");
+include_once ("../../classes/util/ezdate.php");
+include_once ("../../classes/util/logger.php");
+include_once ("../../classes/auth/RegnSession.php");
+include_once ("../../classes/auth/Master.php");
+include_once ("../../classes/import/personimportclass.php");
+include_once ("../../classes/import/personimportpersisterclass.php");
+
+
+$delimiter = array_key_exists("delimiter", $_REQUEST) ? $_REQUEST["delimiter"] : "";
+
+header('Content-type: octet-stream');
+header('Content-Disposition: attachment; filename="personexport.csv"');
+
+
+$db = new DB();
+$logger = new Logger($db);
+$regnSession = new RegnSession($db);
+$regnSession->auth();
+
+    
+$prep = $db->prepare("select id as memberid, firstname,lastname,email,address,postnmb,city,country,phone,cellphone,employee,birthdate,newsletter, gender from ".AppConfig::pre()."person where hidden=0");
+$res = $prep->execute();
+
+$headerExported = 0;
+
+foreach($res as $one) {
+
+    $keys = array_keys($one);
+
+    if(!$headerExported) {
+        $headerExported = 1;
+        echo implode(",", $keys)."\n";
+    }
+
+
+    $first = 1;
+    foreach($keys as $field) {
+
+
+        if($first) {
+            $first = 0;
+        } else {
+            echo ",";
+        }
+
+        switch($field) {
+            case "newsletter":
+            case "employee":
+                echo $one[field];
+                break;
+            case "birthdate":
+                $tmpdate = new eZDate();
+                $tmpdate->setMySQLDate($one["birthdate"]);
+                echo "\"".$tmpdate->displayAccount()."\"";
+                break;
+            default:
+                echo "\"".$one[$field]."\"";
+                break;
+        }
+    }
+    echo "\n";
+}
+?>

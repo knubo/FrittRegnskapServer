d15946be05eba297b5b6b8045944a6af78f970eb
knutbo <knutbo@7b885804-ab2d-0410-b250-0b2d121ae3f5>
2010-07-08 14:11:49 +0000

    These files are not needed nomore.
    git-svn-id: https://accountserver.googlecode.com/svn/trunk@297 7b885804-ab2d-0410-b250-0b2d121ae3f5

--- RegnskapServer/BuildServerZip.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-zip -r AccountServer.zip classes conf pdf services test.php -x "*.svn*"
--- RegnskapServer/test.php
+++ RegnskapServer/test.php
@@ -1,10 +1,2 @@
-<?php
-/*
- * Created on Apr 1, 2007
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
-?>
 
 <?php phpinfo()?>

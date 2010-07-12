985df0740805ca3a3173e119aa1f70575309ddb9
knutbo <knutbo@7b885804-ab2d-0410-b250-0b2d121ae3f5>
2010-07-12 22:28:00 +0000

    Work with structured massletter editor.
    git-svn-id: https://accountserver.googlecode.com/svn/trunk@299 7b885804-ab2d-0410-b250-0b2d121ae3f5

--- RegnskapServer/services/reports/massletter.php
+++ RegnskapServer/services/reports/massletter.php
@@ -31,7 +31,7 @@ $prefix = $regnSession->getPrefix();
 $standard = new AccountStandard($db);
 
 if (!$year) {
-	$year = $standard->getOneValue(AccountStandard::CONST_YEAR);
+    $year = $standard->getOneValue(AccountStandard::CONST_YEAR);
 }
 
 $accPrices = new AccountMemberPrice($db);
@@ -47,24 +47,62 @@ set_time_limit(1800);
 
 $massLetterHelper = new MassLetterHelper($db, $year, $yearprice, $courseprice, $trainprice, $dueDate, $prefix, $regnSession);
 switch ($action) {
-	case "pdf" :
-		$massLetterHelper->useTemplate($template);
-		break;
-	case "preview":
-	    $massLetterHelper->useTemplate($template, 1);
-	    system(AppConfig::CONVERT." ../../storage/$prefix/massletter_preview.pdf ../../storage/$prefix/massletter_preview.png");
-	    header("Content-Type: image/png");
-	    readfile("../../storage/$prefix/massletter_preview.png");
-	    break;
-	case "list" :
+    case "pdf" :
+        $massLetterHelper->useTemplate($template);
+        break;
+    case "fonts":
+        $arr = array();
+        foreach(scandir("../../pdf/fonts/") as $one) {
+            if($one[0] == '.') {
+                continue;
+            }
+            $arr[] = substr($one, 0, -4);
+        }
+        usort($arr, function($a, $b) {
+            $al = strtolower($a);
+            $bl = strtolower($b);
+            if ($al == $bl) {
+                return 0;
+            }
+            return ($al > $bl) ? +1 : -1;
+        });
+        
+        echo json_encode($arr);
+        break;
+
+    case "images":
+        $arr = array();
+        foreach(scandir("../../storage/$prefix/") as $one) {
+            if($one[0] == '.') {
+                continue;
+            }
+            $type = substr($one, -4);
+
+            if($type != ".png" && $type != ".gif" && $type != ".jpg" && $type != "jpeg") {
+                continue;
+            }
+
+            $arr[] = $one;
+        }
+        sort($arr);
+        echo json_encode($arr);
+        break;
+         
+    case "preview":
+        $massLetterHelper->useTemplate($template, 1);
+        system(AppConfig::CONVERT." ../../storage/$prefix/massletter_preview.pdf ../../storage/$prefix/massletter_preview.png");
+        header("Content-Type: image/png");
+        readfile("../../storage/$prefix/massletter_preview.png");
+        break;
+    case "list" :
         echo json_encode($massLetterHelper->listTemplates());
         break;
-	case "source":
-		echo $massLetterHelper->readTemplate($template);
-	    break;
-	case "save":
-		echo $massLetterHelper->saveTemplate($template, $data);
-	    break;
+    case "source":
+        echo $massLetterHelper->readTemplate($template);
+        break;
+    case "save":
+        echo $massLetterHelper->saveTemplate($template, $data);
+        break;
     default:
         die("Unknown action $action");
 }
